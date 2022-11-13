09131fe1ef4b427cc7b5d7098d57b8bea0a80707
Yan Doroshenko <yan1994doroshenko@gmail.com>
2022-11-13 22:48:36 +0100

    [wip] WIP: Chapter 11 excercises

--- 11_algebraic_data_types/excercises.hs
+++ 11_algebraic_data_types/excercises.hs
@@ -39,4 +39,15 @@ capitalizeParagraph = foldl f "" . words
           | isSuffixOf "." l = l ++ " " ++ capitalizeWord r
           | otherwise = l ++ " " ++ r
 
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit n) = n
+eval (Add l r) = (eval l) + (eval r)
+
+printExpr :: Expr -> String
+printExpr (Lit n) = show n
+printExpr (Add l r) = (printExpr l) ++ " + " ++ (printExpr r)
 
