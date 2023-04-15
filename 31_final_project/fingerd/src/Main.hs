d253d8c203609f3b32566221fba6c91285d8e416
Yan Doroshenko <yan1994doroshenko@gmail.com>
2023-04-15 21:41:09 +0200

    [wip] WIP: Final project

---
+++ 31_final_project/fingerd/src/Main.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Database.SQLite.Simple (Connection)
+import qualified Database.SQLite.Simple as SQLite
+import Network.Socket
+import Data.Text.Encoding (decodeUtf8)
+import Network.Socket.ByteString (recv)
+
+import Lib
+
+handleQuery :: Connection -> Socket -> IO ()
+handleQuery dbConn soc = do
+  msg <- recv soc 1024
+  case msg of
+    "\r\n" -> returnUsers dbConn soc
+    name ->
+      returnUser dbConn soc
+      (decodeUtf8 name)
+
+handleQueries :: Connection -> Socket -> IO ()
+handleQueries = handle handleQuery
+
+main :: IO ()
+main = withSocketsDo $ do
+  addrinfos <-
+    getAddrInfo
+      (Just (defaultHints
+        {addrFlags = [AI_PASSIVE]}))
+      Nothing (Just "79")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr)
+    Stream defaultProtocol
+  bind sock (addrAddress serveraddr)
+  listen sock 1
+  -- only one connection open at a time
+  conn <- SQLite.open "finger.db"
+  handleQueries conn sock
+  SQLite.close conn
+  close sock
